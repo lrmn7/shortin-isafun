eaf83e5474cf8d729d69cd530c723b200651347c
L RMN <97253349+lrmn7@users.noreply.github.com>
2023-05-18 23:04:45 +0700

    Update next-seo.config.js

--- next-seo.config.js
+++ next-seo.config.js
@@ -14,7 +14,7 @@ const defaultSEOConfig = {
     type: "website",
     images: [
       {
-        url: "https://og.yehezgun.com/api/base?description=The%20URL%20is%20too%20long%3F%20Make%20it%20short%21&logo=https%3A%2F%2Fwww.google.com%2Furl%3Fsa%3Di%26url%3Dhttps%253A%252F%252Fwww.pngwing.com%252Fen%252Ffree-png-aazqj%26psig%3DAOvVaw2PaWJ6-AxCGxWODitGgt11%26ust%3D1683964846301000%26source%3Dimages%26cd%3Dvfe%26ved%3D0CBEQjRxqFwoTCMDh0oio7_4CFQAAAAAdAAAAABAF&logoWidth=0&siteName=shortin.is-a.fun&templateTitle=SHORTIN&theme=dark",
+        url: "https://cdn.discordapp.com/attachments/1091192083852828744/1108785976458879098/Screenshot_2023-05-18_at_22-59-15_L_RMN_Shortin_Personalized_Link_Shortener.png",
         alt: "shortin.is-a.fun og-image",
         width: 800,
         height: 600,
